3d3c7f0437949f57a95d5e7f26222e4b454bc748
BulgarianDynamite <101887045+BulgarianDynamite@users.noreply.github.com>
2025-08-15 11:59:20 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function toggleMode() {
-    const body = document.body;
-    const button = document.querySelector('.mode-toggle');
-    body.classList.toggle('light');
-    body.classList.toggle('dark');
-    button.textContent = body.classList.contains('dark') ? '☀️' : '🌙';
-}
